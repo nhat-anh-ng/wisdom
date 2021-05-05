4a8d208c8543222c3ec6605c276035794854749c
Nhat <nhatanh1nguyen@gmail.com>
2021-05-05 19:27:39 +0200

    form

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import Post from "./Post/Post";
 import useStyles from "./styles";
 const Posts = () => {
-  const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+  //const classes = useStyles();
+
+  console.log(posts);
   return (
     <>
       <h1>Posts</h1>
